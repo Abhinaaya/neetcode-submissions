3ce921563724ae04ae6e8f95692cfac29a372f59
Nunna Sri Abhinaya <155607598+Abhinaaya@users.noreply.github.com>
2026-07-27 05:46:24 +0530

    Add: find-minimum-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        heap=[]
+        for num in nums:
+            heapq.heappush(heap,-num)
+            if len(heap)==2:
+                heapq.heappop(heap)
+        return -heap[0]
+        
